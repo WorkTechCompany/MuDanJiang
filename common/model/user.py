cbf599c38ef924bda9174950a625b601839f7ee8
任鸣 <renming@lianzai.com>
2019-01-16 10:07:48 +0800

    增加登录注册与用户账户密码字段

---
+++ common/model/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.schema import FetchedValue
+from application import db
+
+
+class user(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(100), nullable=False, server_default=FetchedValue())
+    userpassword = db.Column(String(128), nullable=False, server_default=FetchedValue())
