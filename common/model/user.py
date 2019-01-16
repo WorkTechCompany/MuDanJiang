5a0cfee589b613fa291d84946a0c915a3f07ee82
任鸣 <renming@lianzai.com>
2019-01-16 10:38:52 +0800

    添加邮箱字段

--- common/model/user.py
+++ common/model/user.py
@@ -9,3 +9,4 @@ class user(db.Model):
     id = db.Column(Integer, primary_key=True)
     username = db.Column(String(100), nullable=False, server_default=FetchedValue())
     userpassword = db.Column(String(128), nullable=False, server_default=FetchedValue())
+    mail = db.Column(String(128), nullable=False, server_default=FetchedValue())
